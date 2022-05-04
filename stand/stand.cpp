a90cc8e0ec12f3fc1d6d525bd556790b89a8f093
ortfero <ortfero@gmail.com>
2022-05-04 15:32:33 +0300

    Test project to find deadlock

--- stand/stand.cpp
+++ stand/stand.cpp
@@ -1,18 +1,33 @@
+#include <cassert>
+#include <chrono>
 #include <cstdio>
+#include <string>
+#include <thread>
 
 #include <hydra/activity.hpp>
 
 
 int main(int, char**) {
 
-    hydra::activity<int> activity;
-    activity.reserve(8192);
-    auto const run_result = activity.run([](auto&) {
+    hydra::activity<std::string> activity;
+    activity.reserve(2);
+    auto const run_result = activity.run([](auto& batch) {
+        for(auto seq = batch.try_fetch(); !!seq; seq = batch.try_fetch()) {
+            std::fprintf(stdout, "%s\n", batch[seq].data());
+            batch.fetched();
+        }
     });
 
     if(!run_result) {
         std::fprintf(stderr, "Unable to start activity\n");
         return -1;
+    }
+    
+    for(int i = 0; i != 4000000; ++i) {
+        auto const seq = activity.claim();
+        assert(!!seq);
+        activity[seq] = std::to_string(i);
+        activity.publish(seq);
     }
 
     std::fprintf(stdout, "Press ENTER to stop...\n");
